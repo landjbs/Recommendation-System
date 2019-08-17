f075a5ef635e2dbe1963c2b8e62d8a4b264e4665
Landon Smith <landjbs@gmail.com>
2019-08-17 16:00:55 -0400

    combined title finding

--- preprocessing/userProcessing.py
+++ preprocessing/userProcessing.py
@@ -7,14 +7,17 @@ def process_user_data(userPath):
     userDict = {}
     with open(userPath, 'r') as userFile:
         for line in tqdm(userFile):
-            firstCommaLoc = line.find(',')
-            userId = line[:firstCommaLoc]
-            userData = line[(firstCommaLoc + 1):]
-            if userId in userDict:
-                userDict[userId].append(userData)
+            if ':' in line:
+                print(line)
             else:
-                userDict.update({userId : [userData]})
-    save('data/outData/userDict.sav', userDict)
+                firstCommaLoc = line.find(',')
+                userId = line[:firstCommaLoc]
+                userData = line[(firstCommaLoc + 1):]
+                if userId in userDict:
+                    userDict[userId].append(userData)
+                else:
+                    userDict.update({userId : [userData]})
+    save(userDict, 'data/outData/userDict.sav')
     while True:
-        i = int(input("id: "))
+        i = (input("id: "))
         print(userDict[i])
--- runner.py
+++ runner.py
@@ -2,6 +2,14 @@
 
 # process_movie_titles('data/inData/movie_titles.txt')
 
+from utils.objectSaver import load
+
+# userDict = load('data/outData/userDict.sav')
+# print(userDict['262367'])
+#
+# while True:
+#     i = input("id: ")
+#     print(userDict[i])
 
 from preprocessing.userProcessing import process_user_data
 
